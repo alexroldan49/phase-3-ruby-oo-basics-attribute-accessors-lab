37ac8c5f7714956f8d1c0b3794259f4358a2b32d
Alexander Roldan <alexroldan49@gmail.com>
2021-09-05 09:56:26 -0400

    commit tree cb8ff525f2d87b889cdd84db9ba932c8dd811313

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,25 @@
 ## code your solution here. 
+require 'pry'
+
+class Cat
+
+    attr_accessor name
+
+    def initialize(name)
+    @name = name
+    end
+
+    def name
+        puts @name
+    end
+
+    def meow
+        print "meow"
+    end 
+
+end 
+
+midnight = Cat.new("midnight")
+midnight.name
+midnight.meow
+
